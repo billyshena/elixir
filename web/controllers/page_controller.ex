020c29abb0ef884bcff100a52f377ca3acee4ab4
Billy Shen <billy.shen@hetic.net>
2016-06-28 23:15:22 +0200

    update

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule ElixirApp.PageController do
   use ElixirApp.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+  |> send_resp(201, "loaderio-8bb397278d712441ad9d9dab8f444bab")
   end
 end
